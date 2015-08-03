df83248266876b7266bd975eeea37748036ad862
J <jack@tnj.zone>
2015-08-03 15:49:00 +1000

    change position to bottom left instead of following the mouse

--- URL_popup.js
+++ URL_popup.js
@@ -11,8 +11,7 @@ Alert.Init = function () {
 Alert.Attach = function () {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener("mouseover", URL.Append, false);
-        links[i].addEventListener("mousemove", Alert.Move, false);
+        links[i].addEventListener("mouseover", Alert.Append, false);
         links[i].addEventListener("mouseout", Alert.Remove, false);
     }
 };
@@ -20,52 +19,37 @@ Alert.Attach = function () {
 Alert.Create = function () {
     Alert.toolTip = document.createElement("div");
     Alert.toolTip.id = "link_alert_tooltip";
-    Alert.toolTip.style.position = "absolute";
+    Alert.toolTip.style.position = "fixed";
     Alert.toolTip.style.zIndex = 10000000; // make sure the tooltip is above everything else
     Alert.toolTip.style.lineHeight = "100%";
-    Alert.toolTip.style.padding = "10px";
+    Alert.toolTip.style.padding = "10px 0 5px 10px";
     Alert.toolTip.style.boxSizing = "border-box";
-    Alert.toolTip.style.margin = "15px 15px 15px 0";
+    Alert.toolTip.style.margin = "0";
     Alert.toolTip.style.background = "rgba(255, 255, 255, 0.85)";
+    Alert.toolTip.style.bottom = "0";
+    Alert.toolTip.style.left = "0";
+    Alert.toolTip.style.textAlign = "left";
+    Alert.text = document.createElement("span");
+    Alert.toolTip.appendChild(Alert.text);
+    document.body.appendChild(Alert.toolTip);
 };
-// set icon container position relative to the mouse cursor
-Alert.Move = function (e) {
-    if (Alert.on == true) {
-        Alert.toolTip.style.left = e.pageX + "px";
-        Alert.toolTip.style.top = e.pageY + "px";
-    }
-};
+
 // remove icon container from the DOM
 Alert.Remove = function () {
     if (Alert.on == true) {
-        document.body.removeChild(Alert.toolTip);
+        Alert.toolTip.style.display = "none";
         Alert.on = false;
-        URL.Remove();
     }
 };
 // append icon container to the DOM
 Alert.Append = function (e) {
-    if (Alert.on == true) // remove previous container if it is stuck
+    if (Alert.on == false) // remove previous container if it is stuck
     {
-        document.body.removeChild(document.getElementById("link_alert_tooltip"));
+        Alert.text.innerHTML = this.getAttribute('href');
+        Alert.toolTip.style.display = "block";
+        Alert.on = true;
     }
-    Alert.on = true;
-    Alert.Move(e); // set icon container position
-    document.body.appendChild(Alert.toolTip);
-};
-var URL = {};
-URL.Append = function (e) {
-    var hrefStr = this.getAttribute('href');
-    var href = document.createElement("span");
-    href.innerHTML = hrefStr;
-    this.removeAttribute("title");
-    Alert.toolTip.appendChild(href);
-    Alert.Append(e);
-};
-URL.Remove = function () {
-    for (var i = Alert.toolTip.childNodes.length - 1; i >= 0; i--) {
-        Alert.toolTip.removeChild(Alert.toolTip.childNodes[i]);
-    }
+
 };
 
 Alert.Init();
